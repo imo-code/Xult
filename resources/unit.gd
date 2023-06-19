8e8fcc1b1dc2d0610c845bfd791a949b15608c4f
imo-code <45245303+imo-code@users.noreply.github.com>
2023-06-18 23:23:16 -0400

    added unit.gd
    ability to make simple characters with textures.
    added three example characters.

---
+++ resources/unit.gd
@@ -0,0 +1,36 @@
+extends Resource
+class_name Unit
+
+@export var title:  String
+@export var max_hp: int
+@export var c_hp:   int
+@export var manah:  int
+@export var tags: 	Array
+@export var spells: Array
+@export var actions: Array
+@export var tex : Texture2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+	
+func get_tags():
+	return tags
+	
+func get_spells():
+	return spells
+	
+func get_max_hp():
+	return max_hp
+
+func get_current_hp():
+	return c_hp
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+	
+
+
